129e20772635de830e9de0b105824199fe0be0e4
Padmaja <109962029+PadmajaKothapalli@users.noreply.github.com>
2022-11-28 15:48:13 +0530

    Add files via upload

---
+++ Basic Practice/Assignments on Lang Package/Reversestring.java
@@ -0,0 +1,7 @@
+class StringFormatter{
+	public static String reverseString(String str){
+		StringBuilder sb= new StringBuilder(str);
+		sb.reverse();
+		return sb.toString();
+	}
+}
---
+++ Basic Practice/Assignments on Lang Package/TestStringFormatter.java
@@ -0,0 +1,6 @@
+class TestStringFormatter{
+	public static void main(String[] args){
+		System.out.println(StringFormatter.reverseString("My name is Padmaja"));
+		System.out.println(StringFormatter.reverseString("I am Indian"));
+	}
+}
