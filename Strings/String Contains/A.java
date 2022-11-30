2a9c955c1ba2d219c88dd8c94fba5e84403fff70
Padmaja <109962029+PadmajaKothapalli@users.noreply.github.com>
2022-11-30 15:27:12 +0530

    Add files via upload

---
+++ Strings/String Contains/A.java
@@ -0,0 +1,9 @@
+class A{
+	public static void main(String[] args){
+		String name="What do you know about me";
+		System.out.println(name.contains("do you know"));
+		System.out.println(name.contains("do"));
+		System.out.println(name.contains("name"));
+		System.out.println(name.contains("What"));
+	}
+}
---
+++ Strings/String Contains/using If-else/A.java
@@ -0,0 +1,11 @@
+public class A{
+	public static void main(String[] args){
+		String str= "Java string contains() method using if-else condition";
+		if(str.contains("Java")){
+			System.out.println("This string contains Java");
+		}
+		else{
+			System.out.println("String not found");
+		}
+	}
+}
