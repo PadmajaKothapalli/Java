53ed7ad944baf3ed38a3a24df5ed8c0bd9a9d2bb
Padmaja <109962029+PadmajaKothapalli@users.noreply.github.com>
2022-12-23 21:12:33 +0530

    Add files via upload

---
+++ Java2/Exception Handling/Example/Test.java
@@ -0,0 +1,11 @@
+class Test{
+	public static void main(String[] args){
+		//int i= Integer.parseInt("Lavanya");
+		//System.out.println(i);
+		
+		String ename="Pavithra Kothapalli";
+		String ename=null;
+		System.out.println(ename.length());
+		
+	}
+}
